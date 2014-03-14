339f5e026fc463ae63ef6ede6b578d2dbdbc554c
Emilien Kenler <ekenler@wizcorp.jp>
2014-03-14 10:43:58 +0900

    Init angular app with generator-angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('packageApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
