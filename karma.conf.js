b5a4ce72783548455df244b4103f08e9f8d3ea8d
Emilien Kenler <ekenler@wizcorp.jp>
2014-03-14 11:10:06 +0900

    Use karma-nodewebkit-launcher

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function(config) {
     basePath: '',
 
     // testing framework to use (jasmine/mocha/qunit/...)
-    frameworks: ['mocha'],
+    frameworks: ['jasmine'],
 
     // list of files / patterns to load in the browser
     files: [
@@ -46,7 +46,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
+    browsers: ['NodeWebkit'],
 
 
     // Continuous Integration mode
